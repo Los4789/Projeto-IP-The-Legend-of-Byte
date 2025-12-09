50daeef3471bc1cc580020fd461dd7dee38f0ff2
Los4789 <los4@cin.ufpe.br>
2025-12-09 16:51:56 -0300

    Update Esqueleto.py

--- Esqueleto.py
+++ Esqueleto.py
@@ -1,10 +1,13 @@
 import pygame, sys
 from configuracao import *
+from level import Level
 class Game:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption('The Legend of Byte')
         self.clock = pygame.time.Clock()
+        self.level = Level()
     def run(self):
         while True:
             for event in pygame.event.get():
@@ -12,6 +15,7 @@ class Game:
                     pygame.quit()
                     sys.exit()
             self.screen.fill('black')
+            self.level.run()
             pygame.display.update()
             self.clock.tick(FPS)
 if __name__ == '__main__':
