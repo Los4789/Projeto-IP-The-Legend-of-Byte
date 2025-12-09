db0bc7f8553301bed5a3c97e6402d71f012c44e3
Los4789 <los4@cin.ufpe.br>
2025-12-09 16:56:07 -0300

    Update level.py

--- level.py
+++ level.py
@@ -1,6 +1,7 @@
 import pygame
 class Level:
   def __init__(self):
+    self.display_surface = pygame.display.get_surface()
     self.spritesvisiveis = pygame.sprite.Group()
     self.spritesobstaculos = pygame.sprite.Group()
   def run(self):
