3f9a5afe5dea95c70c1bed85435ea32bc23c8416
Los4789 <los4@cin.ufpe.br>
2025-12-13 13:31:02 -0300

    Update main.py

--- jogo/main.py
+++ jogo/main.py
@@ -8,14 +8,25 @@ class Game:
         pygame.display.set_caption('The Legend of Byte')
         self.clock = pygame.time.Clock()
         self.level = Level(self.screen)
+        self.game_duration = 60000 # 60 segundos
+        self.start_time = pygame.time.get_ticks()
+        self.game_active = True
     def run(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-            self.screen.fill('black')
-            self.level.run()
+            if self.game_active:
+                current_time = pygame.time.get_ticks()
+                time_left_ms = self.game_duration - (current_time - self.start_time)
+                if time_left_ms <= 0:
+                    time_left_ms = 0
+                    self.game_active = False # Fim de jogo
+                self.screen.fill('black')
+                self.level.run(int(time_left_ms / 1000))
+            else:
+                self.screen.fill('gray') 
             pygame.display.update()
             self.clock.tick(FPS)
 if __name__ == '__main__':
