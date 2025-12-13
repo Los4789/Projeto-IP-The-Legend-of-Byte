299ca53ea28f5994c804e22cff65d11ed12631c6
Los4789 <los4@cin.ufpe.br>
2025-12-13 12:35:00 -0300

    Update level.py

--- jogo/level.py
+++ jogo/level.py
@@ -1,30 +1,36 @@
 import pygame
-from tile import Tile 
 from ui import UI
 from coletaveis import Coletavel
 from player import Player
 from configuracao import *
 # Camera
 class CameraGroup(pygame.sprite.Group):
-    def __init__(self):
+    def __init__(self, map_width, map_height):
         super().__init__()
         self.display_surface = pygame.display.get_surface()
         self.offset = pygame.math.Vector2()
-        self.half_w = self.display_surface.get_size()[0] // 2
-        self.half_h = self.display_surface.get_size()[1] // 2
-        # Fundo para ajudar a visualizar o movimento da câmera
-        self.floor_surf = pygame.Surface((WIDTH * 2, HEIGHT * 2)) # Um grande fundo preto
-        self.floor_surf.fill('black')
+        self.half_w = self.display_surface.get_size() // 2
+        self.half_h = self.display_surface.get_size() // 2
+        self.floor_surf_original = pygame.image.load('graphics/tilemap/ground.png').convert()
+        num_tiles_x = int(map_width / self.floor_surf_original.get_width()) + 1
+        num_tiles_y = int(map_height / self.floor_surf_original.get_height()) + 1
+        self.floor_surf = pygame.Surface((num_tiles_x * self.floor_surf_original.get_width(), 
+                                          num_tiles_y * self.floor_surf_original.get_height()))
+        for x in range(num_tiles_x):
+            for y in range(num_tiles_y):
+                self.floor_surf.blit(self.floor_surf_original, (x * self.floor_surf_original.get_width(), 
+                                                               y * self.floor_surf_original.get_height()))
+                
         self.floor_rect = self.floor_surf.get_rect(topleft = (0, 0))
-    def custom_draw(self, player):
+     def custom_draw(self, player):
         self.offset.x = player.rect.centerx - self.half_w
         self.offset.y = player.rect.centery - self.half_h
-        # fundo com offset
         floor_offset_pos = self.floor_rect.topleft - self.offset
         self.display_surface.blit(self.floor_surf, floor_offset_pos)
         for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
             offset_pos = sprite.rect.topleft - self.offset
             self.display_surface.blit(sprite.image, offset_pos)
+
 class Level:
     def __init__(self, surface):
         self.display_surface = surface 
