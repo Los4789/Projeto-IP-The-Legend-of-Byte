32dbc256fd708c02d02c170d5117e9123f30561d
Los4789 <los4@cin.ufpe.br>
2025-12-13 12:07:21 -0300

    Update support.py

--- jogo/support.py
+++ jogo/support.py
@@ -1,1 +1,16 @@
+import pygame
+from os import walk
 
+def import_folder(path):
+    surface_list = []
+    
+    for _, __, img_files in walk(path):
+        # Garante que os frames sejam processados em ordem numérica (000, 001, etc.)
+        img_files = sorted(img_files) 
+        
+        for image_name in img_files:
+            full_path = path + '/' + image_name
+            image_surf = pygame.image.load(full_path).convert_alpha()
+            surface_list.append(image_surf)
+            
+    return surface_list
