44fbfcebb7b25e2d6df3a558f51a33c90bf023ed
Los4789 <los4@cin.ufpe.br>
2025-12-13 13:20:51 -0300

    Update tile.py

--- jogo/tile.py
+++ jogo/tile.py
@@ -1,9 +1,9 @@
 import pygame
-from configuracao import TILESIZE
+from configuracao import TILESIZE 
 class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
-        self.image = pygame.Surface((TILESIZE, TILESIZE))
-        self.image.fill('white') 
+        self.image = pygame.image.load('graphics/tilemap/bush.png').convert_alpha()
+        self.image = pygame.transform.scale(self.image, (TILESIZE, TILESIZE))
         self.rect = self.image.get_rect(topleft=pos)
-        self.hitbox = self.rect.inflate(0, 0) 
+        self.hitbox = self.rect.inflate(0, 0)
