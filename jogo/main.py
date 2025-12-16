762d6e226c826cefb98df9956b878a3a8e1a5925
Los4789 <los4@cin.ufpe.br>
2025-12-16 10:09:55 -0300

    Update main.py

--- jogo/main.py
+++ jogo/main.py
@@ -5,86 +5,85 @@ class Game:
     def __init__(self):
         pygame.mixer.init() 
         pygame.init()
-        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
-        self.display_surface = pygame.display.get_surface()
+        self.Screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.DisplaySurface = pygame.display.get_surface()
         pygame.display.set_caption('The Legend of Byte')
-        self.clock = pygame.time.Clock()
-        self.music_game = 'music/Chiptune Original.mp3'
-        self.music_game_over = 'music/game_over_music.ogg'
-        self.start_game()
-        self.game_over_surf = pygame.image.load('graphics/game_over_screen.png').convert_alpha()
-        self.game_over_rect = self.game_over_surf.get_rect(center=(WIDTH/2, HEIGHT/2))
-        self.font = pygame.font.Font(UI_FONT, UI_FONT_SIZE * 2) 
-    def start_game(self):
-        self.level = Level(self.screen)
-        self.game_duration = 15000 
-        self.start_time = pygame.time.get_ticks()
-        self.game_active = True
-        self.final_score_data = None
-        self.play_music(self.music_game, -1, 0.5)
+        self.Clock = pygame.time.Clock()
+        self.MusicGame = 'music/Chiptune Original.mp3'
+        self.MusicGameOver = 'music/game_over_music.ogg'
+        self.StartGame()
+        self.GameOverSurf = pygame.image.load('graphics/game_over_screen.png').convert_alpha()
+        self.GameOverRect = self.GameOverSurf.get_rect(center=(WIDTH/2, HEIGHT/2))
+        self.Font = pygame.font.Font(UI_FONT, UI_FONT_SIZE * 2) 
+    def StartGame(self):
+        self.Level = Level(self.Screen)
+        self.GameDuration = 15000 
+        self.StartTime = pygame.time.get_ticks()
+        self.GameActive = True
+        self.FinalScoreData = None
+        self.PlayMusic(self.MusicGame, -1, 0.5)
         pygame.event.clear()
-    def play_music(self, path, loops=-1, volume=0.5):
+    def PlayMusic(self, Path, Loops=-1, Volume=0.5):
         try:
-            pygame.mixer.music.load(path)
-            pygame.mixer.music.play(loops)
-            pygame.mixer.music.set_volume(volume)
-            print(f"Tocando música: {path}")
+            pygame.mixer.music.load(Path)
+            pygame.mixer.music.play(Loops)
+            pygame.mixer.music.set_volume(Volume)
+            print(f"Tocando música: {Path}")
         except pygame.error as e:
-            print(f"Erro ao carregar ou tocar música {path}: {e}")
-    def run(self):
+            print(f"Erro ao carregar ou tocar música {Path}: {e}")
+    def Run(self):
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+            for Event in pygame.event.get():
+                if Event.type == pygame.QUIT:
                     pygame.mixer.music.stop()
                     pygame.quit()
                     sys.exit()
-                if event.type == ADD_TIME_EVENT:
-                    # Só adiciona tempo se o jogo estiver ativo
-                    if self.game_active: 
-                        self.start_time += event.amount
-                if event.type == pygame.KEYDOWN:
-                    if not self.game_active:
-                        if event.key == pygame.K_r:
-                            self.start_game()
-                        if event.key == pygame.K_s:
+                if Event.type == ADD_TIME_EVENT:
+                    if self.GameActive: 
+                        self.StartTime += Event.amount
+                if Event.type == pygame.KEYDOWN:
+                    if not self.GameActive:
+                        if Event.key == pygame.K_r:
+                            self.StartGame()
+                        if Event.key == pygame.K_s:
                             pygame.mixer.music.stop()
                             pygame.quit()
                             sys.exit()
-            if self.game_active:
-                current_time = pygame.time.get_ticks()
-                time_left_ms = self.game_duration - (current_time - self.start_time)
-                if time_left_ms <= 0:
-                    time_left_ms = 0
-                    self.game_active = False 
-                    self.final_score_data = self.level.score 
+            if self.GameActive:
+                CurrentTime = pygame.time.get_ticks()
+                TimeLeftMs = self.GameDuration - (CurrentTime - self.StartTime)
+                if TimeLeftMs <= 0:
+                    TimeLeftMs = 0
+                    self.GameActive = False 
+                    self.FinalScoreData = self.Level.score 
                     pygame.mixer.music.stop() 
-                    self.play_music(self.music_game_over, -1, 0.6) 
-                self.screen.fill('black')
-                self.level.run(int(time_left_ms / 1000))
+                    self.PlayMusic(self.MusicGameOver, -1, 0.6) 
+                self.Screen.fill('black')
+                self.Level.run(int(TimeLeftMs / 1000))
             else:
-                if self.game_over_surf:
-                    self.screen.blit(self.game_over_surf, self.game_over_rect)
+                if self.GameOverSurf:
+                    self.Screen.blit(self.GameOverSurf, self.GameOverRect)
                 else:
-                    self.screen.fill('gray')  
-                if self.final_score_data:
-                    total_score = sum(self.final_score_data.values())
-                    score_text = f'Score Final: {total_score}'
-                    score_surface = self.font.render(score_text, True, TEXT_COLOR)
-                    score_rect = score_surface.get_rect(topright=(WIDTH - 10, 10))
-                    pygame.draw.rect(self.screen, 'black', score_rect.inflate(10, 10), 0, 5)
-                    self.screen.blit(score_surface, score_rect)
-                restart_text = 'R = Recomeçar'
-                restart_surface = self.font.render(restart_text, True, TEXT_COLOR)
-                restart_rect = restart_surface.get_rect(bottomleft=(5, HEIGHT - 5))
-                quit_text = 'S = Sair'
-                quit_surface = self.font.render(quit_text, True, TEXT_COLOR)
-                quit_rect = quit_surface.get_rect(topleft=(5, 5))
-                pygame.draw.rect(self.screen, 'black', quit_rect.inflate(10, 10), 0, 5)
-                self.screen.blit(quit_surface, quit_rect)
-                pygame.draw.rect(self.screen, 'black', restart_rect.inflate(10, 10), 0, 5)
-                self.screen.blit(restart_surface, restart_rect)
+                    self.Screen.fill('gray')  
+                if self.FinalScoreData:
+                    TotalScore = sum(self.FinalScoreData.values())
+                    ScoreText = f'Score Final: {TotalScore}'
+                    ScoreSurface = self.Font.render(ScoreText, True, TEXT_COLOR)
+                    ScoreRect = ScoreSurface.get_rect(topright=(WIDTH - 10, 10))
+                    pygame.draw.rect(self.Screen, 'black', ScoreRect.inflate(10, 10), 0, 5)
+                    self.Screen.blit(ScoreSurface, ScoreRect)
+                RestartText = 'R = Recomeçar'
+                RestartSurface = self.Font.render(RestartText, True, TEXT_COLOR)
+                RestartRect = RestartSurface.get_rect(bottomleft=(5, HEIGHT - 5))
+                QuitText = 'S = Sair'
+                QuitSurface = self.Font.render(QuitText, True, TEXT_COLOR)
+                QuitRect = QuitSurface.get_rect(topleft=(5, 5))
+                pygame.draw.rect(self.Screen, 'black', QuitRect.inflate(10, 10), 0, 5)
+                self.Screen.blit(QuitSurface, QuitRect)
+                pygame.draw.rect(self.Screen, 'black', RestartRect.inflate(10, 10), 0, 5)
+                self.Screen.blit(RestartSurface, RestartRect)
             pygame.display.update()
-            self.clock.tick(FPS)
+            self.Clock.tick(FPS)
 if __name__ == '__main__':
-    game = Game()
-    game.run()
+    MyGame = Game()
+    MyGame.Run()
